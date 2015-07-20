933dc087826e698b0d29ffb54a0c2eb75e722cc4
lucy-orbach <lucy.orbach@gmail.com>
2015-07-20 09:19:04 -0400

    changes done for SEO: file names, a links, meta data, title

--- js/main.js
+++ js/main.js
@@ -1,54 +1,20 @@
-function fadePage(){
+"use strict";
+
+$(function fadePage(){
 	$('html').hide().fadeIn(800);
-}
-fadePage();
-
-
-// ********  F  U  N  C  T  I  O  N  :  scrollToAnchor **
-//performs an slow motion scroll to Anchor
-var linkToAnchor = [];
-var myAnchor = [];
-
-function scrollToAnchor (linkToAnchor,myAnchor) {
-	linkToAnchor.click( function(){
-		 $('html,body').animate( {scrollTop: myAnchor.offset().top} , 1000 );
-	});
-
-};
-
-scrollToAnchor ( $('.navbar-brand'), $('#branding') )
-scrollToAnchor ( $('#goServices'), $('#services') )
-scrollToAnchor ( $('#goAbout'), $('#about') )
-scrollToAnchor ( $('#goContact'), $('#contact') )
-scrollToAnchor ( $('#goWall'), $('#caseWall') )
-
-
-
-// ***** OPEN FOOTER MENU MOBILE ****
-
-$('footer').click ( function(){
-	$('footer').toggleClass('openUp');
 });
 
-//****** CAROUSEL   ****************
 
-$('.carousel').carousel({
-  interval: 15000
-})
-
+$("footer").click(function(){
+	$(this).toggleClass('openUp');
+});
 
 
-// //***** toggle Case Studies menu ****
 
-// $('#toggleMenu').click ( function(){
-// 	$('.caseMenu').toggleClass('open');
-// });
 
-// $('#caseTab').mouseover ( function(){
-// 	$('.caseMenu').addClass('show');
-// });
-// $('.caseContent').click (function(){
-// 	$('.caseMenu').removeClass('show');
-// });
+
+
+
+
 
 
